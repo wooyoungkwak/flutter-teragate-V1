4879f3dc5b09d7a7027a8aadfa29a8c59b383de7
wooyoungkwak <zilet123456.gmail.com>
2022-07-26 10:48:29 +0900

    IOS Notification 설정 수정

--- lib/utils/alarm_util.dart
+++ lib/utils/alarm_util.dart
@@ -42,36 +42,6 @@ void _showDialog(BuildContext context, String title, var contentWigets, var acti
 }
 
 void showConfirmDialog(BuildContext context, String title, String text) {
-  // showDialog(
-  //     context: context,
-  //     barrierDismissible: false, //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)), // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
-  //         title: Column(
-  //           children: const <Widget>[
-  //             Text("Dialog Title"),
-  //           ],
-  //         ), //Dialog Main Title
-  //         content: Column(
-  //           mainAxisSize: MainAxisSize.min,
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: <Widget>[
-  //             Text(text),
-  //           ],
-  //         ),
-  //         actions: <Widget>[
-  //           TextButton(
-  //             child: const Text("ok"),
-  //             onPressed: () {
-  //               Navigator.pop(context);
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     }
-  // );
-
   _showDialog(
     context, 
     title, 
@@ -88,39 +58,7 @@ void showConfirmDialog(BuildContext context, String title, String text) {
 
 }
 
-
 void showOkCancelDialog(BuildContext context, String title, String text, Function okCallback) {
-  // showDialog(
-  //     context: context,
-  //     barrierDismissible: false, // 바깥 영역 터치시 닫을지 여부
-  //     builder: (BuildContext context) {
-  //       return AlertDialog(
-  //         title: Text(title),
-  //         content: SingleChildScrollView(
-  //           child: Column(
-  //             children: <Widget>[
-  //               Text(text),
-  //             ],
-  //           ),
-  //         ),
-  //         actions: <Widget>[
-  //           TextButton(
-  //             child: const Text('ok'),
-  //             onPressed: () {
-  //               okCallback();
-  //             },
-  //           ),
-  //           TextButton(
-  //             child: const Text('cancel'),
-  //             onPressed: () {
-  //               Navigator.pop(context);
-  //             },
-  //           ),
-  //         ],
-  //       );
-  //     }
-  //   );
-
   _showDialog(
     context, 
     title, 
@@ -141,8 +79,6 @@ void showOkCancelDialog(BuildContext context, String title, String text, Functio
     ]
   );
 }
-
-
 
 Future showNotification(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin, String tag, String subtitle) async {
 
